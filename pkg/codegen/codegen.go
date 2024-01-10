0c600d0e8952bf5298662ea0907c3dba2367e9be
Vasil <vasiltopalovic@outlook.com>
2024-01-10 15:08:39 -0500

    feat: generate expression to bytes

--- pkg/codegen/codegen.go
+++ pkg/codegen/codegen.go
@@ -43,12 +43,15 @@ func generateCode(node Instruction) []byte {
 	out := []byte{}
 	switch node := node.(type) {
 	case Procedure:
+		//return type
 		out = append(out, []byte(node.ReturnType.CType())...)
 		out = append(out, []byte(" ")...)
+
+		//name
 		out = append(out, []byte(node.Ident)...)
 		out = append(out, []byte("(")...)
-		//args
 
+		//args
 		for i, arg := range node.Args {
 			out = append(out, []byte(arg.Type.CType())...)
 			out = append(out, []byte(" ")...)
@@ -60,8 +63,8 @@ func generateCode(node Instruction) []byte {
 
 		out = append(out, []byte(")")...)
 		out = append(out, []byte(" {\n")...)
-		//body
 
+		//body
 		for _, instruction := range node.Instructions {
 			out = append(out, generateCode(instruction)...)
 		}
@@ -70,8 +73,25 @@ func generateCode(node Instruction) []byte {
 	case Escape:
 		out = append(out, []byte("\treturn")...)
 		out = append(out, []byte(" ")...)
-		out = append(out, []byte("0")...)
+
+		out = append(out, GenerateExpr(node.Expr)...)
+
 		out = append(out, []byte(";\n")...)
+	}
+
+	return out
+
+}
+
+func GenerateExpr(node Expr) []byte {
+
+	out := []byte{}
+
+	switch node := node.(type) {
+	case ExprMath:
+		for _, token := range node.Tokens {
+			out = append(out, []byte(token.Value)...)
+		}
 	}
 
 	return out
