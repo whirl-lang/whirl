68d4176a9d6950079cc56422193bf9f8b57e6a28
Vasil <vasiltopalovic@outlook.com>
2024-01-09 23:28:59 -0500

    feat: parser types

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -4,9 +4,55 @@ import (
 	lexer "github.com/whirl-lang/whirl/pkg/lexer"
 )
 
-type Instruction interface {}
+type Instruction interface{}
 
+type Procedure struct {
+	Indentifier  string
+	Instructions []Instruction
+	ReturnType   Type
+}
 
-func CreateAst(tokens []lexer.Token) []Instruction {
+type Struct struct {
+	Fields []Field
+}
+
+type Field struct {
+	Identifier string
+	Type       Type
+}
+
+type If struct {
+	Condition Expression
+	Body      []Instruction
+}
+
+type Expression interface {
+	Value() Literal
+}
+
+type Assignment struct {
+	Identifier string
+	Expression Expression
+}
+
+type FunctionCall struct {
+	Identifier string
+	Arguments  []Expression
+}
+
+type Escape struct {
+	Expression Expression
+}
+
+type Literal interface{}
+
+// Types
+type Type interface{}
+type Int struct{}
+type String struct{}
+type Bool struct{}
+type Char struct{}
+
+func Iterator(tokens lexer.TokenIterator) []Instruction {
 	return nil
 }
